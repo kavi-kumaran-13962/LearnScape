188fce1993a3cff9ed6c7e27b14a13af3e38e7e4
kavi kumaran <kaviathelete@gmail.com>
2020-12-17 00:55:32 +0530

    minute changes

--- Learn-Web-Dev-Client/src/component/Input/Input.js
+++ Learn-Web-Dev-Client/src/component/Input/Input.js
@@ -1,55 +1,50 @@
-import React, { Component } from 'react'
-import "./Input.scss"
+import React, { Component } from "react";
+import "./Input.scss";
 
-
- class Input extends Component {
-    constructor(props){
-        super(props);
-        
+class Input extends Component {
+  constructor(props) {
+    super(props);
     this.inputClick = this.inputClick.bind(this);
     this.inputLeave = this.inputLeave.bind(this);
     this.state = {
-      input:false,
-    }
-    }
-    inputClick(e) {
-        e.preventDefault();
-        this.setState({ [e.target.title]: true });
-      }
-      inputLeave(e) {
-        e.preventDefault();
-        this.setState({ [e.target.title]: false });
-      }
-    render() {
-        return (
-          <div> 
-          <div
+      input: false,
+    };
+  }
+  inputClick(e) {
+    e.preventDefault();
+    this.setState({ [e.target.title]: true });
+  }
+  inputLeave(e) {
+    e.preventDefault();
+    this.setState({ [e.target.title]: false });
+  }
+  render() {
+    return (
+      <div>
+        <div
           className={this.props.title}
-          title= {this.props.title}
+          title={this.props.title}
           onMouseEnter={this.inputClick}
           onMouseLeave={this.inputLeave}
           style={{
-            boxShadow:`${this.props.boxShadow}`,
-            height:`${this.props.height}`  ,
-            width: `${this.props.width}`
+            boxShadow: `${this.props.boxShadow}`,
+            height: `${this.props.height}`,
+            width: `${this.props.width}`,
           }}
         >
           <i class={this.props.icon}></i>
-          
+
           <input
             autoComplete="off"
             className="input__inp "
-            placeholder = {this.props.title}
+            placeholder={this.props.title}
             id={this.props.title}
             type="text"
             name={this.props.title}
-           
           />
         </div>
-        
-          </div>
-            
-        )
-    }
+      </div>
+    );
+  }
 }
 export default Input;
