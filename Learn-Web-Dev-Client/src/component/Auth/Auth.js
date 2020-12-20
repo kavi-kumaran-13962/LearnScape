6ac566431abf295fb5b355f79c1ba1ef495b59b2
kavi kumaran <kaviathelete@gmail.com>
2020-12-20 20:56:06 +0530

    input rebuilded

--- Learn-Web-Dev-Client/src/component/Auth/Auth.js
+++ Learn-Web-Dev-Client/src/component/Auth/Auth.js
@@ -1,54 +1,32 @@
 import React, { Component } from "react";
 import logo from "./logo.png";
 import developer from "./developer.svg";
-import Input from "../Input/Input"
-import Button from "../Button/Button"
 import "./Auth.scss";
-export default class Auth extends Component {
-  
-  constructor() {
-    super();
-    this.state= {
-      style:{
-          height: '2rem',
-          width: '19rem',
-          title:["email","password","Login", "SignUp"],
-          boxShadow :"0px 3px 7px #2983c4",
-          icon:["fa fa-user-o","fa fa-lock"]
-        }
-      }
-  }  
-render() {
-    return (
-      <div className="Auth">
-        <div className="Auth__container">
-          <div className="Auth__image">
-            <img className="Auth__image--img" src={developer} alt="developer" />
-          </div>
-          <div className="Auth__form">
-            <div className="Auth__form--logo">
-              <div className="Auth__form__image">
-                <img className="Auth__form--img" src={logo} alt="logo" />
-              </div>
-              <div className="Auth__form--logoText">
-                <h3 className="Auth__form--logotxt">Learn Web Dev</h3>
-              </div>
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+
+export default function Auth() {
+  return (
+    <div className="Auth">
+      <div className="Auth__container">
+        <div className="Auth__image">
+          <img className="Auth__image--img" src={developer} alt="developer" />
+        </div>
+        <div className="Auth__form">
+          <div className="Auth__form--logo">
+            <div className="Auth__form__image">
+              <img className="Auth__form--img" src={logo} alt="logo" />
             </div>
-            <Input  height = {this.state.style.height} 
-                    width = {this.state.style.width}
-                    title = {this.state.style.title[0]}
-                    boxShadow = {this.state.style.boxShadow}
-                    icon = {this.state.style.icon[0]}/>
-            <Input  height = {this.state.style.height} 
-                    width = {this.state.style.width}
-                    title = {this.state.style.title[1]}
-                    boxShadow = {this.state.style.boxShadow}
-                    icon = {this.state.style.icon[1]}/>
-            <Button title ={this.state.style.title[2]}/>     
-            <Button title ={this.state.style.title[3]}/>   
+            <div className="Auth__form--logoText">
+              <h3 className="Auth__form--logotxt">Learn Web Dev</h3>
+            </div>
           </div>
+          <Input title="Email" icon="fa fa-user" />
+          <Input title="Password" icon="fa fa-lock" />
+          <Button title="Login" />
+          <Button title="Signup" />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
