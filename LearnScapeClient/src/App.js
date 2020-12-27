15ab847399482168d1dc571d7a102c1344e0b442
kavi kumaran <kaviathelete@gmail.com>
2020-12-27 20:35:06 +0530

    small changes

--- LearnScapeClient/src/App.js
+++ LearnScapeClient/src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./styles/utility.scss";
 import StudentLogin from "./component/Login/Student/Student";
 import TeacherLogin from "./component/Login/Teacher/Teacher";
 import CcLogin from "./component/Login/Cc/Cc";
 import Home from "./component/Home/Home";
-export default function App() {
+function App() {
   return (
     <div>
-      <switch>
-        <Route path="/" component={Home} />
-        <Route path="/cc" component={CcLogin} />
-        <Route path="/teacher" component={TeacherLogin} />
-        <Route path="/student" component={StudentLogin} />
-      </switch>
+      <Switch>
+        <Route path="/" component={StudentLogin} />
+      </Switch>
     </div>
   );
 }
+export default App;
